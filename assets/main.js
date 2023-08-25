1d99054d47c7c6cbfe444c10110dd62ada3bec14
FedericaCampo97 <federica.campo97@gmail.com>
2023-08-25 16:07:19 +0200

    Exercise complete

--- assets/main.js
+++ assets/main.js
@@ -2,14 +2,17 @@
 let element = document.getElementById ('number');
 
 //Creo un ciclo
-for (let i = 1; i < 15; i++) {
+for (let i = 1; i < 101; i++) {
     //Verifico se i numeri sono multipli di 3 e di 5
-    if (Number.isInteger(i/3 && i/5)) {
-        console.log();
+    if (Number.isInteger(i/3) && Number.isInteger(i/5)) {
+        console.log('Buzz-Fizz');
+    //Altrimenti se è divisibile per 3
     } else if (Number.isInteger(i/3)) {
         console.log('Fizz');
+    //Altrimenti se è divisibile per 5
     } else if (Number.isInteger(i/5)) {
         console.log('Buzz');
+    //Altrimenti
     } else {
         console.log (i)
     }
