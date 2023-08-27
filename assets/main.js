e62aeb99ebb257449c908ecf290a86f05fbe5ce6
FedericaCampo97 <federica.campo97@gmail.com>
2023-08-27 23:21:24 +0200

    Bonus2

--- assets/main.js
+++ assets/main.js
@@ -8,24 +8,26 @@ for (let i = 1; i < 101; i++) {
     let element = document.createElement('div');
     if (Number.isInteger(i/3) && Number.isInteger(i/5)) {
         console.log('BuzzFizz');
-        element.append('BuzzFizz')
-        
+        element.innerHTML = 'BuzzFizz'
+        element.style.background = "red";
         div.append(element)
 
     //Altrimenti se è divisibile per 3
     } else if (Number.isInteger(i/3)) {
         console.log('Fizz');
-        element.append('Fizz')
+        element.innerHTML = 'Fizz'
+        element.style.background = "yellow"
         div.append(element)
     //Altrimenti se è divisibile per 5
     } else if (Number.isInteger(i/5)) {
         console.log('Buzz');
-        element.append('Buzz')
+        element.innerHTML = 'Buzz'
+        element.style.background = "orange"
         div.append(element)
     //Altrimenti
     } else {
         console.log (i)
-        element.append(i)
+        element.innerHTML = i
         div.append(element)
 
     }
