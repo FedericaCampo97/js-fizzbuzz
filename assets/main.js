7915bc5023f14c3aa543325a85599cd64a382f6b
FedericaCampo97 <federica.campo97@gmail.com>
2023-08-27 23:13:24 +0200

    bonus1

--- assets/main.js
+++ assets/main.js
@@ -4,16 +4,32 @@ let element = document.getElementById ('number');
 //Creo un ciclo
 for (let i = 1; i < 101; i++) {
     //Verifico se i numeri sono multipli di 3 e di 5
+    const div = document.querySelector("div.square")
+    let element = document.createElement('div');
     if (Number.isInteger(i/3) && Number.isInteger(i/5)) {
-        console.log('Buzz-Fizz');
+        console.log('BuzzFizz');
+        element.append('BuzzFizz')
+        
+        div.append(element)
+
     //Altrimenti se è divisibile per 3
     } else if (Number.isInteger(i/3)) {
         console.log('Fizz');
+        element.append('Fizz')
+        div.append(element)
     //Altrimenti se è divisibile per 5
     } else if (Number.isInteger(i/5)) {
         console.log('Buzz');
+        element.append('Buzz')
+        div.append(element)
     //Altrimenti
     } else {
         console.log (i)
+        element.append(i)
+        div.append(element)
+
     }
+
+    
 }
+
